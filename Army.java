3c06b6f28008bcbf17fa9c862c26fa4bfd8d7869
Ashira Mawji <ashiramawji@vlan402-208.subnet-241.amherst.edu>
2018-12-02 13:48:35 -0500

    h

--- Army.java
+++ Army.java
@@ -1,13 +1,22 @@
 public class Army {
   private final int OWNER;
-  private int COUNTRY;
+  private int country;
   private final Color MYARMYCOLOR;
+  private int armyPosX;
+  private int armyPosY;
 
   public Army (int owner, int country) {
     this.OWNER = owner;
     this.COUNTRY = country;
     this.MYARMYCOLOR = playerArray[owner].getMyColor();
+    this.armyPosX = countryArray[country].POSX;
+    this.armyPosY = countryArray[country].POSY;
   }
+
+  public void setCountry (int a) {
+    this.country = a;
+  }
+
 
 
 }

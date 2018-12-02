e2c2298ab8e1b0f645044e8e4f3e970f91ae6cdb
Ashira Mawji <ashiramawji@vlan402-208.subnet-241.amherst.edu>
2018-12-02 11:15:59 -0500

    Initial methods

--- Main.java
+++ Main.java
@@ -1,1 +1,54 @@
-Main method
+
+public class Main {
+
+  //Initialize playerArrayList
+  ArrayList<Player> playerArrayList = new ArrayList<Player>;
+
+
+  public static void Main (String[] args) {
+
+    //Instantiate playerArrayList
+    for (int i = 0; i < numPlayers; i++) {
+      playerArrayList.add(new Player);
+    }
+
+    while (GAMEOVER == false) {
+      play(playerArrayList.get(i));
+      if (i == numPlayers) {
+        i = 0;
+      }
+      else {
+        i++;
+      }
+    }
+  }
+
+  public void play (Player p) {
+
+  }
+
+  public void collectArmies() {
+
+  }
+
+  public void cashCards() {
+
+  }
+
+  public void placeArmies() {
+
+  }
+
+  public void attack() {
+
+  }
+
+  public void moveArmies() {
+
+  }
+
+  public void endTurn () {
+    
+  }
+
+}

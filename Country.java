d3ddf099d7693eb8d35139cb931bd1209d0aaf7e
Ashira Mawji <ashiramawji@vlan402-208.subnet-241.amherst.edu>
2018-12-11 21:30:26 -0500

    Merge branch 'master' of https://github.com/duckduckgogogo/FinalProject

--- Country.java
+++ Country.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -118,3 +119,119 @@ public class Country {
 
 
 }
+=======
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Country {
+  //private int playerInControl;
+  private int numArmies;
+  private final int CONTINENT;
+  //final boolean CAPITAL;
+  final String NAME;
+  private final int POSX;
+  private final int POSY;
+  private final int COUNTRYHEIGHT;
+  private final int COUNTRYWIDTH;
+  private final Color MYCOLOR;
+  private Color armyColor;
+  private int owner = 10; 
+  final private int ARRAYPOS;
+
+  public Country (String b, int d, int e, int width, int height, int continent) /*int[] a*/ {
+    this.numArmies = 0;
+    this.CONTINENT = continent;
+    this.COUNTRYHEIGHT = height;
+    this.COUNTRYWIDTH = width;
+    this.NAME = b;
+    //this.MYCOLOR = c;
+    this.POSX = d;
+    this.POSY = e;
+    this.MYCOLOR = Color.BLUE;
+    this.ARRAYPOS = 0;
+
+    //Set color based on continent
+
+    /*this.CONTINENT = a[0].parseInt();
+    this.NAME = a[1];
+    this.MYCOLOR = a[2].parseColor();
+    this.POSX = a[3].parseInt();
+    this.POSY = a[4].parseInt();*/
+    //this.owner = e;
+  }
+
+  public int getArrayPos () {
+    return ARRAYPOS;
+  }
+
+  public int getOwner () {
+    return owner;
+  }
+
+  public void setOwner (int i) {
+    owner = i;
+  }
+
+  public int getNumArmies () {
+    return numArmies;
+  }
+
+  public void addArmy (int a) {
+    numArmies += a;
+  }
+
+  public void subtractArmy (int a) {
+    numArmies -= a;
+  }
+
+  public int getContinent () {
+    return CONTINENT;
+  }
+
+  public int getPosX () {
+    return POSX;
+  }
+
+  public int getPosY () {
+    return POSY;
+  }
+
+  public String getName () {
+    return NAME;
+  }
+
+  public Color getColor () {
+    return MYCOLOR;
+  }
+  
+  public int getWidth() {
+	  return COUNTRYWIDTH;
+  }
+  
+  public int getHeight() {
+	  return COUNTRYHEIGHT;
+  }
+
+  public void setArmyColor (Color c) {
+    armyColor = c;
+  }
+  
+  public void drawCountry(Graphics g) {
+    g.setColor(MYCOLOR);
+    g.fillRect(POSX, POSY, COUNTRYWIDTH, COUNTRYHEIGHT);
+    g.setColor(armyColor);
+  }
+
+  public boolean isIn (int x, int y) {
+    if ((x >= POSX) && (x <= (POSX+COUNTRYWIDTH)) && (y >= POSY) && (y <= (POSY + COUNTRYHEIGHT))) {
+      return true;
+    }
+    return false;
+  }
+
+
+
+
+
+}
+>>>>>>> 5620678d3f6d128b466764a94a4202638a97217b
